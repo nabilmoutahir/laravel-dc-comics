48e51c2c082910d264d7864cedab5ca5ba6cece4
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-31 19:36:12 +0200

    add migrate and seeder

--- database/migrations/2024_03_31_164416_create_comics_table.php
+++ database/migrations/2024_03_31_164416_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+
             $table->timestamps();
         });
     }
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// IMPORT APP MODEL
+use App\Models\comic;
+
+// IMPORT FAKER
+use Faker\Generator as Faker;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,8 +18,22 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $newComic = new Comic;
+
+            // ADD PARAMETERS
+            $newComic->title = $faker->text(50);
+            $newComic->description = $faker->paragraphs(3, true);
+            $newComic->thumb = $faker->imageUrl(640, 480, 'comic', true);
+            $newComic->price = $faker->text(10);
+            $newComic->series = $faker->sentence();
+            $newComic->sale_date = $faker->date();
+            $newComic->type = $faker->text(30);
+
+            // SAVE
+            $newComic->save();
+        }
     }
 }
