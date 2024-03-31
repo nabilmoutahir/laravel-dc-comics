b589539012020c8600810394dae2bafb85f0117c
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-31 23:49:45 +0200

    add delete form

--- resources/views/COMICS/index.blade.php
+++ resources/views/COMICS/index.blade.php
@@ -18,6 +18,7 @@
                         <th>Type</th>
                         <th>More</th>
                         <th class="px-3">Edit</th>
+                        <th>Delete</th>
 
                     </tr>
                 </thead>
@@ -30,8 +31,18 @@
                             <td>{{ $comic->series }}</td>
                             <td>{{ $comic->sale_date }}</td>
                             <td>{{ $comic->type }}</td>
+
                             <td><a href="{{ route('comics.show', $comic) }}">Details</a></td>
+
                             <td class="px-3"><a href="{{ route('comics.edit', $comic) }}">Edit</a></td>
+
+                            <td>
+                                <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button>Delete</button>
+                                </form>
+                            </td>
                         </tr>
                     @empty
                         <tr>
